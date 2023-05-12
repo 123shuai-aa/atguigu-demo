2a09d357501cb95c61637b604ff396694cb3ca93
Layne <Layne@atguigu.com>
2023-05-12 16:33:26 +0800

    first commit01

---
+++ atguigu-Git/src/main/java/com/cn/git/TestGit.java
@@ -0,0 +1,17 @@
+package com.cn.git;
+
+/**
+ * ClassName: TestGit
+ * Package: com.cn.git
+ * Description:
+ *
+ * @Author xxx
+ * @Create 2023/5/12 15:58
+ * @Version 1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+        System.out.println("测试Git");
+    }
+}
